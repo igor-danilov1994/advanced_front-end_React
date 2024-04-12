00f7a88dc6bb8674b53ec044a27b24db71990d4e
igor <igor.danilov1824@gmail.com>
2024-04-12 10:45:21 +0300

    fix webpack plugins file

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,18 +8,25 @@ import { BuildOptions } from './types/config';
 export const buildPlugins = ({
     paths,
     isDev,
-}: BuildOptions): webpack.WebpackPluginInstance[] => [
-    new HTMLWebpackPlugin({
-        template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
-    new webpack.DefinePlugin({
-        __IS_DEV__: JSON.stringify(isDev),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshPlugin(),
-];
+}: BuildOptions): webpack.WebpackPluginInstance[] => {
+    const plugins = [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new ReactRefreshPlugin(),
+    ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+    }
+
+    return plugins;
+};
