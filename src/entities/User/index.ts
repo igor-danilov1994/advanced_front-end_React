32209bfeffcfd35167dcc9c8ba4d0e31c3ef03d9
Igor <58389072+igor-danilov1994@users.noreply.github.com>
2024-09-05 14:53:38 +0300

    Update index.ts
    fake commit for test

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -3,3 +3,4 @@ export { User, UserSchema } from './modal/types/user';
 export { getAuthData } from '../User/modal/selectors/getAuthData/getAuthData';
 export { getInited } from '../User/modal/selectors/getInited/getInited';
 export { userReducer } from './modal/slice/userSlice';
+export { userReducer } from './modal/slice/userSlice';
